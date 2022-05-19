45d3ba1c34a7356ed599180ae71ce587383f8fb3
toni-santos <a.oliveirasantos122@gmail.com>
2022-05-19 03:17:19 +0100

    working on styling for login and signup pages

--- login.php
+++ login.php
@@ -8,16 +8,18 @@ drawTop(["restaurants", "commons", "login"], ["hamburger"]);
     <div id="box">
         <p class="h5">Login</p>
         <form action="/includes/login.inc.php" method="POST">
-            <div>
-                <input class="text" type="text" name="email" placeholder="Email">
-                <label for="email">Email</label>
-                <span>Not a valid email address</span>
-            </div>
-            <div>
-                <input class="text" type="password" name="pwd" placeholder="Password" autocomplete="current-password" minlength="8" required>
-                <label for="email">Email</label>
-                <span>Not a valid password</span>
-            </div>
+            <section id="inputs-box">
+                <div class="input-container">
+                    <input class="text text-input subtitle2" type="email" name="email" autocomplete="email" placeholder=" " required>
+                    <label class="body2 dark-bg" for="email">Email</label>
+                    <span class="error subtitle2">Required</span>
+                </div>
+                <div class="input-container">
+                    <input class="text text-input subtitle2" type="password" name="pwd" placeholder=" " autocomplete="current-password" minlength="8" required>
+                    <label class="body2 dark-bg" for="pwd">Password</label>
+                    <span class="error subtitle2">Required</span>
+                </div>
+            </section>
             <?php
             //checking if the session 'error' is set
             if (isset($_SESSION['error'])) {
@@ -34,7 +36,6 @@ drawTop(["restaurants", "commons", "login"], ["hamburger"]);
         <p class="body2 acc-create">Don't have an account? <a class="body1" href="signup.php">Sign-Up</a> now!</p>
     </div>
 </section>
-
 
 <?php
 drawFooter();
