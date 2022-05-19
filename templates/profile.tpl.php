90280d4689b3c8ed992c4904db84582eb879e02d
toni-santos <a.oliveirasantos122@gmail.com>
2022-05-19 23:16:25 +0100

    started working on content of profile pages

--- templates/profile.tpl.php
+++ templates/profile.tpl.php
@@ -24,48 +24,56 @@ function profileTop()
 function profileBottom(array $tabs, int $scrollVal, Pages $page)
 { ?>
     <div id="profile-bottom">
-        <section id="tabs" class="h6">
-            <?php for ($i = 0; $i < count($tabs); $i++) { ?>
-                <p class="shadow" id="tab-<?php echo $i; ?>" onclick="snapContent(event, <?php echo $scrollVal; ?>, 'bottom-content', 'vertical')"><?php echo $tabs[$i]; ?></p>
-            <?php } ?>
-        </section>
         <hr class="horizontal-divider solid">
-        <!-- TODO: figure out content formatting -->
-        <?php 
-        
-        switch ($page) {
-            case Pages::RestaurantLogged:?>
+        <div id="content-wrapper">
+            <section id="tabs" class="h6">
+                <?php for ($i = 0; $i < count($tabs); $i++) { ?>
+                    <p class="shadow" id="tab-<?php echo $i; ?>" onclick="snapContent(event, <?php echo $scrollVal; ?>, 'bottom-content', 'vertical')"><?php echo $tabs[$i]; ?></p>
+                <?php } ?>
+            </section>
+            <!-- TODO: figure out content formatting -->
+            <?php
+
+            switch ($page) {
+                case Pages::RestaurantLogged:break;
+                case Pages::Restaurant: ?>
+                    <div id="bottom-content">
+                        <section id="info">
+                            <p id="description" class="body2">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam explicabo neque laudantium, asperiores enim, rem architecto sint vel doloribus reiciendis ex, possimus animi ut iure! Atque quam provident saepe autem.
+                            </p>
+                            <p id="gallery">
+                                <img src="images/placeholder.jpg" class="">
+                                <img src="images/placeholder.jpg" class="">
+                                <img src="images/placeholder.jpg" class="">
+                            </p>
+                            <a class="h5">Where to find us?</a>
+                            <!-- geolocation here maybe? -->
+                            <p class="body2">the location</p>
+                        </section>
+                        <section id="menus">
+
+                        </section>
+                        <section id="reviews">
+
+                        </section>
+                    </div>
                 <?php break;
-            case Pages::Restaurant:?>
-                <div id="bottom-content">
-                    <section id="info">
-        
-                    </section>
-                    <section id="menus">
-        
-                    </section>
-                    <section id="reviews">
-        
-                    </section>
-                </div>
-                <?php break;
-            case Pages::UserLogged: ?>
-                <?php break;
-            case Pages::User: ?>
-                <div id="bottom-content">
-                    <section id="info">
-        
-                    </section>
-                    <section id="menus">
-        
-                    </section>
-                    <section id="reviews">
-        
-                    </section>
-                </div>
-                <?php break;
-        }
-        
-        ?>
+                case Pages::UserLogged: break;
+                case Pages::User: ?>
+                    <div id="bottom-content">
+                        <section id="info">
+
+                        </section>
+                        <section id="menus">
+
+                        </section>
+                        <section id="reviews">
+
+                        </section>
+                    </div>
+            <?php break;
+            } ?>
+        </div>
     </div>
 <?php } ?>
