422bb1b56b6a128f0a4995cb207d33c9273a6736
ShirleyRodrigues <shirleyrodrigues2209@gmail.com>
2022-06-08 20:27:29 +0100

    restaurant class tweaks

--- php/restaurant.php
+++ php/restaurant.php
@@ -5,27 +5,61 @@ require_once("../database/db_loader.php");
 
 class Restaurant {
 
-    public ?int $_restaurantID;
-    public string $_name;
-    public string $_location;
-    public ?string $_opening_time;
-    public ?string $_closing_time;
+    public int $restaurantID;
+    public string $name;
+    public string $location;
+    public ?string $opening_time;
+    public ?string $closing_time;
 
-    public function __construct($restaurantID = NULL, $name, $location, $opening_time = NULL, $closing_time = NULL) {
-
-        if (!isset($restaurantID)) {
-            $db = getDatabase();
-            $stmt = $db->prepare("SELECT max(restaurantID) FROM Restaurant");
-            $stmt->execute();
-
-            $this->_restaurantID = intval($stmt->fetch()['max(restaurantID)']) + 1;
-        } else $this->_restaurantID = $restaurantID;
-        
-        $this->_name = $name;
-        $this->_location = $location;
-        $this->_opening_time = $opening_time;
-        $this->_closing_time = $closing_time;
+    public function __construct(int $restaurantID, string $name, string $location, string $opening_time, string $closing_time) {
+        $this->restaurantID = $restaurantID;
+        $this->name = $name;
+        $this->location = $location;
+        $this->opening_time = $opening_time;
+        $this->closing_time = $closing_time;
     }
+
+    public function getRestaurants(PDO $db, int $count) {
+        $stmt = $db->prepare('SELECT restaurantID, name FROM Restaurant LIMIT ?');
+        $stmt->execute(array($count));
+    
+        $restaurants = array();
+        while ($restaurant = $stmt->fetch()) {
+            $restaurants[] = new Restaurant(
+                $restaurant['restaurantID'], 
+                $restaurant['name'],
+                $restaurant ['location'],
+                $restaurant ['opening_time'],
+                $restaurant ['closing_time']
+            );
+        }
+    
+        return $restaurants;
+
+        // if (!isset($restaurantID)) {
+        //     $db = getDatabase();
+        //     $stmt = $db->prepare("SELECT max(restaurantID) FROM Restaurant");
+        //     $stmt->execute();
+
+        //     $this->restaurantID = intval($stmt->fetch()['max(restaurantID)']) + 1;
+        // } else $this->restaurantID = $restaurantID;
+    }
+
+    public function getRestaurant(PDO $db, int $restaurantID) :Restaurant {
+        $stmt = $db->prepare('SELECT restaurantID, name FROM Restaurant WHERE restaurantID = ?');
+        $stmt->execute(array($restaurantID));
+    
+        $restaurant= $stmt->fetch();
+    
+        return new Restaurant(
+            $restaurant['restaurantID'], 
+            $restaurant['name'],
+            $restaurant ['location'],
+            $restaurant ['opening_time'],
+            $restaurant ['closing_time']
+        );
+    }
+
 
     public function add_to_db(PDO $db) {
 
@@ -34,7 +68,7 @@ class Restaurant {
             VALUES (?, ?, ?, ?, ?)
         ');
 
-        $stmt->execute(array($this->_restaurantID, $this->_name, $this->_location, $this->_opening_time, $this->_closing_time));
+        $stmt->execute(array($this->restaurantID, $this->name, $this->location, $this->opening_time, $this->closing_time));
     
     }
 
